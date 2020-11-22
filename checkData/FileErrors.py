a708341ed86f33cfe494185a5f6023075ab06c2e
mjakubowska <martynajakubowska05@gmail.com>
2020-11-23 00:08:35 +0100

    sprawdzanie aptek, dodanie klasy Pharmacy

--- checkData/FileErrors.py
+++ checkData/FileErrors.py
@@ -5,17 +5,43 @@ import model.Producer as pr
 def check_syntax(input_file):
     with open(input_file, "r") as file:
         line = file.readline()
-        if line != "# Producenci szczepionek (id | nazwa | dzienna produkcja)\n":
+        if line[0] != "# Producenci szczepionek (id | nazwa | dzienna produkcja)\n":
             print(
                 "Błąd w wersie 1. \"{} Poprawnie: \"# Producenci szczepionek (id | nazwa | dzienna produkcja)\"".format(
                     line))
-            n = 1
-        prodRegEx = r"[ ]{0,}(\d)+[ ]{0,}\|+[ ]{0,}([\w\-!@#$%^&*()+=.,<>\[\]\/{}\?\\'\";:~` ]+)\|+[ ]{0,}(\d+)"
+        n = 1
+        places_reg_ex = r"[ ]{0,}(\d)+[ ]{0,}\|+[ ]{0,}([\w\-!@#$%^&*()+=.,<>\[\]\/{}\?\\'\";:~` ]+)\|+[ ]{0,}(\d+)"
         producers = list()
-        for line in file.readlines():
-            if re.match(prodRegEx, line):
-                idn = re.match(prodRegEx, line).group(1)
-                name = re.match(prodRegEx, line).group(2)
-                amount = re.match(prodRegEx, line).group(3)
+        while True:
+            line = file.readline()
+            if re.match(places_reg_ex, line):
+                idn = re.match(places_reg_ex, line).group(1)
+                name = re.match(places_reg_ex, line).group(2)
+                amount = re.match(places_reg_ex, line).group(3)
                 producers.append(pr.Producer(idn, name, amount))
-        print(producers[0].name)
+                n += 1
+            else:
+                break
+        print("wczytano producentów")
+        print(n)
+        if line != "# Apteki (id | nazwa | dzienne zapotrzebowanie)\n":
+            print(
+                "Błąd w wersie {} \"{} Poprawnie: \"# Apteki (id | nazwa | dzienne zapotrzebowanie)\"".format(n + 1,
+                                                                                                              line))
+        pharmacies = []
+        while True:
+            line = file.readline()
+            if re.match(places_reg_ex, line):
+                idn = re.match(places_reg_ex, line).group(1)
+                name = re.match(places_reg_ex, line).group(2)
+                amount = re.match(places_reg_ex, line).group(3)
+                pharmacies.append(pr.Pharmacy(idn, name, amount))
+                n += 1
+            else:
+                break
+        print("wczytano apteki")
+        print(n)
+        if line != "# Apteki (id | nazwa | dzienne zapotrzebowanie)\n":
+            print(
+                "Błąd w wersie {} \"{} Poprawnie: \"# Apteki (id | nazwa | dzienne zapotrzebowanie)\"".format(n + 1,
+                                                                                                              line))
---
+++ model/Pharmacy.py
@@ -0,0 +1,6 @@
+class Pharmacy:
+
+    def __init__(self, idn, name, amount):
+        self.idn = idn
+        self.name = name
+        self.amount = amount
